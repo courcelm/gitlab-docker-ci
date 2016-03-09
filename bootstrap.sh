9cbecd32b62393be3b69019bc681f73f5310f0d0
Brad Jones <brad@jones.name>
2016-03-08 17:03:53 -0700

    Allow for self-registration if config.toml file absent. Add docker-compose file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 
 export CI_USER=gitlab-runner
 /set-docker-group.sh
+if [[ ! -f /etc/gitlab-runner/config/config.toml ]]; then /usr/bin/gitlab-runner register; fi
 /usr/bin/gitlab-runner run --user=${CI_USER} --working-directory=/home/${CI_USER}
-
